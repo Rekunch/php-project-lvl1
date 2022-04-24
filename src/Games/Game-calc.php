745599ee16c0d33499abafebbe0736d31ccbecdb
Rekunch <95749296+Rekunch@users.noreply.github.com>
2022-04-24 18:50:58 +0300

    Update Game-calc.php

--- src/Games/Game-calc.php
+++ src/Games/Game-calc.php
@@ -4,7 +4,7 @@ namespace Games\Calc;
 use Src\Engine ;
 use function welcome;
 use function engineGame;
-use function cli\line; 
+use function cli\line;
 
 $name = Engine\welcome();
 $countRounds = 0;
@@ -14,25 +14,26 @@ while ($countRounds < 3) {
     $rand1 = rand(1, 50);
     $rand2 = rand(1, 50);
     $randSign = rand(1, 3);
-    if($randSign == 1){
+    if ($randSign == 1) {
         $randSign = "+";
         $correctAnswer = $rand1 + $rand2;
     }
-if($randSign == 2){
+    if ($randSign == 2) {
         $randSign = "-";
-     $correctAnswer = $rand1 - $rand2;
-}
-if($randSign == 3){
+        $correctAnswer = $rand1 - $rand2;
+    }
+    if ($randSign == 3) {
             $randSign = "*";
-     $correctAnswer = $rand1 * $rand2;
-}
-$question = "$rand1$randSign$rand2";
+        $correctAnswer = $rand1 * $rand2;
+    }
+    $question = "$rand1$randSign$rand2";
 
-if (Engine\engineGame($question, $correctAnswer))
+    if (Engine\engineGame($question, $correctAnswer))
     $countRounds++;
     else {
-		line('let`s try again, '.$name);
-        break;}
+		line('let`s try again, ' . $name);
+        break;
+    }
 if ($countRounds == 3)
-    line('Congrulations, '.$name);
+    line('Congrulations, ' . $name);
 }
