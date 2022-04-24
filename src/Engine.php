5f484388b0ee37221d1a8b387654a087010ace00
Rekunch <95749296+Rekunch@users.noreply.github.com>
2022-04-24 18:53:01 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -3,20 +3,21 @@
 namespace Src\Engine;
 
 use  Games\Calc;
+
 use function cli\line;
 use function cli\prompt;
 
-function welcome(){
-	line('Welcome to the Brain Game!');
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
-	return $name;
-
+function welcome()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    return $name;
 }
 
 function engineGame($question, $correctAnswer){
 	$currentAnswer = '';
-		line('Question: '.$question);
+		line('Question: ' . $question);
 		$currentAnswer = prompt('Your answer');
 	if ($currentAnswer == $correctAnswer){
 		line('Correct!');
