5e10114afffe5a9c7104efb09b13a11caa03d13a
Rekunch <95749296+Rekunch@users.noreply.github.com>
2022-04-24 18:46:38 +0300

    Update Game-calc.php

--- src/Games/Game-calc.php
+++ src/Games/Game-calc.php
@@ -2,7 +2,6 @@
 namespace Games\Calc;
 
 use Src\Engine ;
-
 use function welcome;
 use function engineGame;
 use function cli\line; 
@@ -10,15 +9,15 @@ use function cli\line;
 $name = Engine\welcome();
 $countRounds = 0;
 line('What is the result of the expression?');
-while ($countRounds < 3){
-$correctAnswer = 0;
-$rand1 = rand(1, 50);
-$rand2 = rand(1, 50);
-$randSign = rand(1, 3);
-if($randSign == 1){
-    $randSign = "+";
-    $correctAnswer = $rand1 + $rand2;
-}
+while ($countRounds < 3) {
+    $correctAnswer = 0;
+    $rand1 = rand(1, 50);
+    $rand2 = rand(1, 50);
+    $randSign = rand(1, 3);
+    if($randSign == 1){
+        $randSign = "+";
+        $correctAnswer = $rand1 + $rand2;
+    }
 if($randSign == 2){
         $randSign = "-";
      $correctAnswer = $rand1 - $rand2;
