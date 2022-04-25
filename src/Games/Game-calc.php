f18c11e9d1757b8b5ee569710be061261388285c
Rekunch <95749296+Rekunch@users.noreply.github.com>
2022-04-25 11:48:10 +0300

    Update Game-calc.php

--- src/Games/Game-calc.php
+++ src/Games/Game-calc.php
@@ -2,6 +2,15 @@
 
 namespace Games\Calc;
 
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
 use Src\Engine ;
 use function welcome;
 use function engineGame;
