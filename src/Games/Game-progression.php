5fa3e35249413e813d76763511cc22a0aefef8fb
Алексей Кривенцов <rekunch1@gmail.com>
2022-04-25 20:25:24 +0300

    add a new game progression

--- src/Games/Game-progression.php
+++ src/Games/Game-progression.php
@@ -13,5 +13,38 @@ function progression()
 	$countRounds = 0;
 	line('What number is missing in the progression?');
 	while ($countRounds < 3) {
+	    $countNumbers = rand(5, 10);
+	    $countProgression = rand (1, 10);
+	    $firstNumber = rand(1, 200);
+	    $unknowNumber = rand (1, $countNumbers);
+	    $arrayNumbers = [];
+	    $i = 0;
+	    while ($i < $countNumbers){
+		    if ($i == 0) {
+			    $arrayNumbers[$i] = $firstNumber;
+			    $i++;
+		    } else { $arrayNumbers[$i] = $arrayNumbers[$i-1] + $countProgression;
+		     $i++;
+		    }
+	    }
+	    $unknowSymbol = '...';
+	    $arrayNumbers[$unknowNumber] = $unknowSymbol;
+	    $correctIndex = array_search($unknowSymbol, $arrayNumbers);
+	    if ($correctIndex > 0) {
+                if($correctIndex <= (count($arrayNumbers))-2)
+			$correctAnswer = ($arrayNumbers[$correctIndex - 1] + $arrayNumbers[$correctIndex + 1]) / 2 ;
+		else $correctAnswer = $arrayNumbers[$correctIndex - 1] + ($arrayNumbers[$correctIndex - 2] - $arrayNumbers[$correctIndex - 3]);
+	    }
+	    $question = implode(' ', $arrayNumbers);
+	    if (Engine\engineGame($question, $correctAnswer))
+		    $countRounds++;
+	    else {
+		    line('Let`s try again, ' . $name);
+		    break;
+	    }
+	    if ($countRounds == 3)
+		    line('Congrulations, ' . $name);
 	}
+	    
+
 }
