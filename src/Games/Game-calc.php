b38f9bcbd2188f3a439335de98128ce679a41c22
Rekunch <95749296+Rekunch@users.noreply.github.com>
2022-04-25 14:06:53 +0300

    fix some linters mistakes

--- src/Games/Game-calc.php
+++ src/Games/Game-calc.php
@@ -7,35 +7,36 @@ use function welcome;
 use function engineGame;
 use function cli\line;
 
-function calc(){
-$name = Engine\welcome();
-$countRounds = 0;
-line('What is the result of the expression?');
-while ($countRounds < 3) {
-    $correctAnswer = 0;
-    $rand1 = rand(1, 50);
-    $rand2 = rand(1, 50);
-    $randSign = rand(1, 3);
-    if ($randSign == 1) {
-        $randSign = "+";
-        $correctAnswer = $rand1 + $rand2;
-    }
-    if ($randSign == 2) {
-        $randSign = "-";
-        $correctAnswer = $rand1 - $rand2;
-    }
-    if ($randSign == 3) {
+function calc()
+{
+    $name = Engine\welcome();
+    $countRounds = 0;
+    line('What is the result of the expression?');
+    while ($countRounds < 3) {
+        $correctAnswer = 0;
+        $rand1 = rand(1, 50);
+        $rand2 = rand(1, 50);
+        $randSign = rand(1, 3);
+        if ($randSign == 1) {
+            $randSign = "+";
+            $correctAnswer = $rand1 + $rand2;
+        }
+        if ($randSign == 2) {
+            $randSign = "-";
+            $correctAnswer = $rand1 - $rand2;
+        }
+        if ($randSign == 3) {
             $randSign = "*";
         $correctAnswer = $rand1 * $rand2;
-    }
-    $question = "$rand1$randSign$rand2";
-    if (Engine\engineGame($question, $correctAnswer))
-    $countRounds++;
-    else {
-        line('let`s try again, ' . $name);
-        break;
-    }
+        }
+        $question = "$rand1$randSign$rand2";
+        if (Engine\engineGame($question, $correctAnswer))
+        $countRounds++;
+        else {
+            line('let`s try again, ' . $name);
+            break;
+        }
     if ($countRounds == 3)
-    line('Congrulations, ' . $name);
-}
+        line('Congrulations, ' . $name);
+    }
 }
