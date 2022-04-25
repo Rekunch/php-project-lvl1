736b656bde713422dc8441e7154c18745f4f85dd
Rekunch <95749296+Rekunch@users.noreply.github.com>
2022-04-25 14:03:04 +0300

    fix linter mistakes Game-gsd.php

--- src/Games/Game-gcd.php
+++ src/Games/Game-gcd.php
@@ -6,25 +6,25 @@ use function welcome;
 use function engineGame;
 use function cli\line;
 
-function gcd(){
-$name = Engine\welcome();
-$countRounds = 0;
-line('Find the greatest common divisor of given numbers.');
-while ($countRounds < 3) {
-    $correctAnswer = 1;
-    $divisor = 1;
-    $randNumber1 = rand(1, 100);
-    $randNumber2 = rand(1, 100);
-    if($randNumber1 > $randNumber2)
-        $correctAnswer = $randNumber2;
-    else 
-           $correctAnswer = $randNumber1;
-    $question = "$randNumber1 $randNumber2";
-    while ($divisor <= $correctAnswer){
-    if($randNumber1 % $correctAnswer !== 0 || $randNumber2 % $correctAnswer !== 0)
-        $correctAnswer--;
-    else 
-      break;
+function gcd()
+{
+    $name = Engine\welcome();
+    $countRounds = 0;
+    line('Find the greatest common divisor of given numbers.');
+    while ($countRounds < 3) {
+        $correctAnswer = 1;
+        $divisor = 1;
+        $randNumber1 = rand(1, 100);
+        $randNumber2 = rand(1, 100);
+        if ($randNumber1 > $randNumber2)
+            $correctAnswer = $randNumber2;
+        else $correctAnswer = $randNumber1;
+        $question = "$randNumber1 $randNumber2";
+        while ($divisor <= $correctAnswer){
+            if ($randNumber1 % $correctAnswer !== 0 || $randNumber2 % $correctAnswer !== 0)
+            $correctAnswer--;
+            else 
+            break;
       }
     if (Engine\engineGame($question, $correctAnswer))
     $countRounds++;
