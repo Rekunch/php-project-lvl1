da4d3f37a72f3b0e71fb391102947dcd9fc9eb90
Алексей Кривенцов <rekunch1@gmail.com>
2022-04-26 12:12:28 +0300

    add a new game prime

--- src/Games/Game-prime.php
+++ src/Games/Game-prime.php
@@ -14,4 +14,30 @@ Function prime()
 	line('Answer "yes" if given number is prime. Otherwise answer "no".');
 	while ($countRounds < 3) {
 		$randNumber = rand(1, 100);
+		$correctAnswerYes = 'yes';
+		$correctAnswerNo = 'no';
+		$i = 1;
+		$question = $randNumber;
+		if ($randNumber == 1)
+			$correctAnswer = $correctAnswerYes;
+		for ($i = 1, $count = 0 ; $i < $question ; $i++){
+			if ($question % $i !== 0)
+				$count = $count;
+		        if ($question % $i == 0)
+				$count++;
+		}
+		if ($count > 1)
+			$correctAnswer = $correctAnswerNo;
+		else $correctAnswer = $correctAnswerYes;
+		if(Engine\engineGame($question, $correctAnswer))
+			$countRounds++;
+		else {
+			line('Let`s try again, ' . $name);
+			break;
+		}
+		if($countRounds == 3)
+			line('Congrulations, ' . $name);
+	}
+}
+
 
